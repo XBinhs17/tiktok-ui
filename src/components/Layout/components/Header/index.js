643b7247a916eea0f4ebddeccfbd32a368c7129b
XBinhs17 <nguyenbinh17062003@gmai.com>
2024-08-09 20:16:38 +0700

    build buttons

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 
+import Button from '~/components/Button';
 import { Wrapper as PopperWrapper} from '~/components/Popper';
 import styles from './Header.module.scss'
 import images  from '~/assets/images';
@@ -55,7 +56,8 @@ function Header() {
             </Tippy>
 
             <div className={cx('actions')}>
-
+                <Button text>Upload</Button>
+                <Button primary>Log in</Button>
             </div>
         </div>
     </header>
